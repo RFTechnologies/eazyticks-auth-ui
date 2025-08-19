f6a162c114f9418179e05c3517b06c29737438d3
rftechnologiespk <rftechnologiespk@gmail.com>
2025-08-19 11:55:14 +0000

    Changes before Firebase Studio auto-run

---
+++ src/lib/site.ts
@@ -0,0 +1,52 @@
+export const Site = {
+    name: "EazyTicks",
+    organizer: {
+      url: "https://organizer.eazyticks.com",
+    },
+    logo: "https://res.cloudinary.com/ddkdvh5ps/image/upload/v1689423314/ezt-templates-assets/Log_Transparent_f5u0p0.png",
+    url: "https://eazyticks.com",
+    SEO_title: "EazyTicks - E-Ticketing Booking Platform",
+    SEO_description:
+      "EazyTicks is an Online E-ticketing platform, easy-to-use and with the lowest ticket booking fees for event creation and selling.",
+    themeColor: "#F85431",
+    socialLinks: {
+      facebook:
+        "https://www.facebook.com/profile.php?id=100087118817848&mibextid=ZbWKwL",
+      twitter: "https://twitter.com/EazyTicks",
+      instagram: "https://instagram.com/eazyticks",
+      youtube: "https://www.youtube.com/@EazyTicks",
+    },
+    code: {
+      ganalytics: "G-ZHXNYLHBEF",
+      gsv: "G8fDfvQLOw4Z0VW109G-31dmZmIrpNAz1ZCmYntrVsA",
+    },
+    placeholder:
+      "https://res.cloudinary.com/ddkdvh5ps/image/upload/v1683022134/ezt-frontend-client-assets/placeholder-image_rkxh0y.png",
+    routes: [
+      {
+        url: "/browse-events",
+      },
+      {
+        url: "/pricing",
+      },
+      {
+        url: "/contact-us",
+      },
+      {
+        url: "/faqs",
+      },
+      {
+        url: "/about-us",
+      },
+      {
+        url: "/features",
+      },
+      {
+        url: "/ticket-finder",
+      },
+      {
+        url: "/testimonials",
+      },
+    ],
+  };
+  
